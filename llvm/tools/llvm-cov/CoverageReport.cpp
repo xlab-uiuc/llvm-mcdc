abb3f012eda722f6aad10b1dca7591f523795435
Wentao Zhang <zhangwt1997@gmail.com>
2024-02-22 23:43:39 -0500

    print number of decisions-2,3,4,5,6 in text mode

--- llvm/tools/llvm-cov/CoverageReport.cpp
+++ llvm/tools/llvm-cov/CoverageReport.cpp
@@ -297,6 +297,16 @@ void CoverageReport::render(const FileCoverageSummary &File,
                  (unsigned)File.MCDCCoverage.getNumPairs());
     OS << format("%*u", FileReportColumns[19],
                  (unsigned)File.MCDCCoverage.getNumDecisions());
+    OS << format("%*u", 4,
+                 (unsigned)File.MCDCCoverage.getNumDecisions2());
+    OS << format("%*u", 4,
+                 (unsigned)File.MCDCCoverage.getNumDecisions3());
+    OS << format("%*u", 4,
+                 (unsigned)File.MCDCCoverage.getNumDecisions4());
+    OS << format("%*u", 4,
+                 (unsigned)File.MCDCCoverage.getNumDecisions5());
+    OS << format("%*u", 4,
+                 (unsigned)File.MCDCCoverage.getNumDecisions6());
     // Options.colored_ostream(OS, LineCoverageColor)
     //     << format("%*u", FileReportColumns[17],
     //               (unsigned)(File.MCDCCoverage.getNumPairs() -
@@ -546,6 +556,11 @@ void CoverageReport::renderFileReports(
                  Column::RightAlignment)
        << column("MC/DC Decisions", FileReportColumns[19],
                  Column::RightAlignment)
+       << column("2", 4, Column::RightAlignment)
+       << column("3", 4, Column::RightAlignment)
+       << column("4", 4, Column::RightAlignment)
+       << column("5", 4, Column::RightAlignment)
+       << column("6", 4, Column::RightAlignment)
        /*<< column("Missed Conditions", FileReportColumns[17],
                  Column::RightAlignment)
        << column("Cover", FileReportColumns[18], Column::RightAlignment)*/;
